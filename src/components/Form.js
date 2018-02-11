229da50504bc27a8b61ad48e63312b9d7e0ff0c5
Craig Wendel <wendelcraig@gmail.com>
2018-02-11 17:15:52 -0600

    Add style to form element on page.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,28 +1,43 @@
 import React, {Component} from 'react'
-import Divider from 'material-ui/Divider'
 import Paper from 'material-ui/Paper'
 import TextField from 'material-ui/TextField'
+import {Card, CardTitle} from 'material-ui/Card'
+import RaisedButton from 'material-ui/RaisedButton'
 
 export default class Form extends Component {
   render () {
-    const style = {
-      height: 350,
+    const cardStyle = {
       width: 1150,
-      margin: 80,
+      marginLeft: 80,
+      marginTop: 10
+    }
+    const style = {
+      height: 400,
+      width: 1150,
+      marginLeft: 80,
+      marginTop: 10,
       padding: 25
+    }
+    const buttonStyle = {
+      margin: 1,
+      marginTop: 15,
+      width: 150,
+      height: 50
     }
 
     return (
       <div className='form'>
+        <Paper zDepth={3} style={cardStyle}>
+          <Card>
+            <CardTitle title='Learn more' subtitle='Sign up below'/>
+          </Card>
+        </Paper>
         <Paper zDepth={3} style={style}>
-          <TextField hintText='First Name' floatingLabelText='First Name' />
-          <Divider />
-          <TextField hintText='Last Name' floatingLabelText='Last Name' />
-          <Divider />
-          <TextField hintText='example@gmail.com' floatingLabelText='Email' />
-          <Divider />
-          <TextField hintText='Must be 8 characters' floatingLabelText='Password' />
-          <Divider />
+          <TextField hintText='First Name' floatingLabelText='First Name' fullWidth='true' />
+          <TextField hintText='Last Name' floatingLabelText='Last Name' fullWidth='true' />
+          <TextField hintText='Your Company Name' floatingLabelText='Company' fullWidth='true' />
+          <TextField hintText='example@gmail.com' floatingLabelText='Email' fullWidth='true' />
+          <RaisedButton label='Submit' primary='true' style={buttonStyle} />
         </Paper>
       </div>
     )
